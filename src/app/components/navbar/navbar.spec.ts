7b1fd2baa23cea1429ec6a60241ba2ba93919c1d
DavidoSF <david-sfeir@hotmail.com>
2025-12-17 17:41:41 +0100

    Test fixes

--- src/app/components/navbar/navbar.spec.ts
+++ src/app/components/navbar/navbar.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore } from '@ngrx/store/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { Navbar } from './navbar';
 
 describe('Navbar', () => {
@@ -9,6 +11,13 @@ describe('Navbar', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Navbar],
+      providers: [
+        provideMockStore(),
+        {
+          provide: ActivatedRoute,
+          useValue: { params: of({}), snapshot: { params: {} } },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Navbar);
